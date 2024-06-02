af92353cf1db1c4bbdbf546d837037e63524512d
ddalpra <d.dalpra86@gmail.com>
2024-06-02 11:45:30 +0200

    Initial commit

---
+++ src/main/java/org/dalpra/acme/entity/Legume.java
@@ -0,0 +1,18 @@
+package org.dalpra.acme.entity;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+public class Legume {
+
+    public String name;
+    public String description;
+
+    public Legume() {
+    }
+
+    public Legume(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
